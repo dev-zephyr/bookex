77e8006d707ef88bda3e78886534bea17d4b9db1
zephyr <bbang1424@naver.com>
2021-02-21 17:33:35 +0900

    oAuth2(Google, Naver) 적용
    ** Naver token관련(?) 오류

--- src/main/java/com/zephyr/bookex/web/IndexController.java
+++ src/main/java/com/zephyr/bookex/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.zephyr.bookex.web;
 
+import com.zephyr.bookex.domain.config.auth.LoginUser;
+import com.zephyr.bookex.domain.config.auth.dto.SessionUser;
 import com.zephyr.bookex.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,16 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            System.out.println("-----------------------------------------");
+            System.out.println(user.getName());
+            System.out.println(user.getEmail());
+        }
 
         return "index";
     }
