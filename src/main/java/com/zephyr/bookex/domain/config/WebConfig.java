57cdf910ea3ef3277dcc41d70534ed4e70d6cb78
zephyr <bbang1424@naver.com>
2021-02-21 17:37:43 +0900

    oAuth2(Google, Naver) 적용
    ** Naver token관련(?) 오류

---
+++ src/main/java/com/zephyr/bookex/domain/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.zephyr.bookex.domain.config;
+
+import com.zephyr.bookex.domain.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
